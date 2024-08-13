fc22b8795ac8cddf2e52ccaf798c3f6dde98cc8b
Madhavilatha <madaviarava@outlook.com>
2024-08-13 19:01:52 +0530

    Time sheet Delete By Id

--- employee-registration/src/Component/TimeSheetComponent.js
+++ employee-registration/src/Component/TimeSheetComponent.js
@@ -241,7 +241,7 @@ function TimeSheetComponent() {
   const deleteData = async (rowData) => {
     debugger;
     try {
-      await UserService.deleteRole(rowData.iroleId);
+      await TimesheetService.deleteTimesheet(rowData.itimeSheetId);
       console.log("Records Deleted successfully");
       window.location.reload();
     } catch (error) {
@@ -472,9 +472,7 @@ function TimeSheetComponent() {
                             >
                               <EditIcon color="primary" />
                             </IconButton>
-                            <IconButton
-                            //onClick={() => handleDelete(row)}
-                            >
+                            <IconButton onClick={() => deleteData(row)}>
                               <DeleteIcon color="error" />
                             </IconButton>
                           </TableCell>
